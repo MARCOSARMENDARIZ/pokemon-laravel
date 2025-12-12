b4843867aa9abb7baae121a67440c692bfa3a58a
Zomber2324 <anscred23@gmail.com>
2025-12-12 11:15:46 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 use App\Http\Controllers\PokemonController;
 
 Route::get('/inicio', [PokemonController::class, 'index'])
